1dec920ba4d85887873912383bdc02b47ac654dc
Trevor Williams <whiltx@gmail.com>
2016-10-08 23:37:18 +0000

    Valid syntax is overrated.

--- src/Enemy/Tank.js
+++ src/Enemy/Tank.js
@@ -6,7 +6,7 @@
 			settings = settings || {};
 			settings.image = 'enemy5';
 			settings.shapes = [ new me.Rect( 0, 0, 24, 24) ];
-			settings.speed 0.2;
+			settings.speed = 0.2;
 			this._super(GBGJ.BaseEnemyEntity, 'init', [x, y, settings]);
 		},
 	});
